f0228c6f22f82b17c95bff6e3f8851dbfbc0e681
Christina <cligare@hotmail.com>
2024-02-18 14:14:54 -0800

    adding questions

--- data.js
+++ data.js
@@ -4,8 +4,9 @@ export const questions = [
   {
     id: 1,
     topic: "HTML",
-    question: "What is html?",
-    answer: "HTML stands for HyperText Markup Language",
+    question: "What is HTML?",
+    answer:
+      "HTML stands for HyperText Markup Language creating the structure of a webpage",
   },
   {
     id: 2,
@@ -18,13 +19,50 @@ export const questions = [
     id: 3,
     topic: "JavaScript",
     question: "What is an arrow function?",
-    answer: "...",
+    answer:
+      "Arrow functions were introduced in ES6. Arrow functions allow us to write shorter function syntax.",
   },
   {
     id: 4,
     topic: "JavaScript",
-    question: "Explain event delegation",
+    question: "What is JSX?",
     answer:
       "JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like",
+  },
+  {
+    id: 5,
+    topic: "JavaScript",
+    question: "What will be returned in this statement? 0 || 'happy day'?",
+    answer:
+      "The logical OR operator will use the first value if it is truthy, i.e., anything apart from NaN, null, undefined, 0, '', and false. Otherwise, it will use the second value. In this case, 'happy day' will be returned.",
+  },
+  {
+    id: 6,
+    topic: "JavaScript",
+    question: "What is hoisting?",
+    answer:
+      "Hoisting in JavaScript is a behavior where variable and function declarations are moved to the top of their containing scope during the compilation phase, before the code is executed. However, it's important to note that hoisting does not work the same way for function expressions (assigned to variables) as it does for function declarations.",
+  },
+  {
+    id: 7,
+    topic: "JavaScript",
+    question: "What is event bubbling?",
+    answer:
+      "Event bubbling is a concept in web development where, when something happens (like a click) to an element on a webpage, that event doesn't just affect that specific element; it also ripples up through its parent elements in the HTML hierarchy.",
+  },
+  {
+    id: 8,
+    topic: "HTML",
+    question:
+      "When was HTML5 introduced and What are the key features of HTML5?",
+    answer:
+      "HTML5 was released in 2014 and introduced many new features like: semantic elements, allowed embedding audio and video content using audio and video tags. Also, the canvas tag allows developers to draw graphics, charts and animations. Also, localStorage was introduced. New input types (email, url, number as well as datalist among others). The viewport meta tag,...and more",
+  },
+  {
+    id: 9,
+    topic: "JavaScript",
+    question: "Explain the difference between var, let and const",
+    answer:
+      "Var is outdated, because it is gloablly scoped and can thus be overwritten. Let is block-scoped and allows reassignment. Const cannot be reassigned. Preferred usage is to use const, unless you need a variable that will be reassigned then use let (for example within a counter, etc.) ",
   },
 ];
--- index.js
+++ index.js
@@ -10,5 +10,29 @@
 // The card should be high and wide enough for the questions and answers to
 //Make sure the contrast is ok
 //filter questions between React, JS, CSS and HTML
+// In one session - if a question has already been asked don't show again.
+
+import { questions } from "./data.js";
 
 const nextQuestionBtn = document.getElementById("next-question-btn");
+const cardContainer = document.getElementById("card-container");
+
+nextQuestionBtn.addEventListener("click", () => {
+  // get random index
+  const randomIndex = Math.floor(Math.random() * questions.length);
+
+  const { question, answer } = questions[randomIndex];
+
+  const cardHTML = `
+  <div class="card">
+    <div class="front">
+      <h2>${question}</h2>
+    </div>
+    <div class="back">
+      <h2>${answer}</h2>
+    </div>
+  </div>
+ `;
+
+  cardContainer.innerHTML = cardHTML;
+});
