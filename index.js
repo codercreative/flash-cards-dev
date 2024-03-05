fdd022c6cf2cbd37b245c04eebffee0f6537c4c1
Christina <cligare@hotmail.com>
2024-03-04 16:34:38 -0800

    modify start page

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ import { questions } from "./data.js";
 const nextQuestionBtn = document.getElementById("next-question-btn");
 const cardContainer = document.getElementById("card-container");
 
+let isFirstLoad = true;
+
 nextQuestionBtn.addEventListener("click", () => {
+  if (isFirstLoad) {
+    nextQuestionBtn.textContent = "Next Question";
+    isFirstLoad = false;
+  }
   // get random index
   const randomIndex = Math.floor(Math.random() * questions.length);
 
-  const { question, answer } = questions[randomIndex];
+  const { question, topic, answer } = questions[randomIndex];
 
   const cardHTML = `
   <div class="card" id="card">
     <div class="front">
+    <p>${topic}</p>
       <h2>${question}</h2>
     </div>
     <div class="back">
+      
       <h2>${answer}</h2>
     </div>
   </div>
